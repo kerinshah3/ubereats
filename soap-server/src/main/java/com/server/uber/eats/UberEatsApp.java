f474a4e8213f8f0cdc56767adf23f9b47cb1e11a
kerin shah <kerinshah@kerins-MacBook-Pro-2.local>
2022-03-15 11:20:38 -0400

    working with docker compose

--- soap-server/src/main/java/com/server/uber/eats/UberEatsApp.java
+++ soap-server/src/main/java/com/server/uber/eats/UberEatsApp.java
@@ -5,11 +5,12 @@ import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 // @EnableJpaRepositories(transactionManagerRef = "platformTransactionManager")
 public class UberEatsApp {
 
---
+++ soap-server/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into customer (first_name , last_name, email) values ('kerin','shah','kerinshah3@gmail.com');
+insert into customer (first_name , last_name, email) values ('Nisarg','shah','NisargShah3@gmail.com');
+insert into customer (first_name , last_name, email) values ('Hardik','shah','Hardikshah3@gmail.com');
+
+insert into address (address_type,city,country,line1,postal_code,province,street_no) values ('Restaurant Address','montreal','canada','nothing','h4e2j7','QC','1835');
+
+insert into restaurant(name,description,address_id) values ('grill indian' ,'authenticate indian restaurant', 1);
+
+insert into dish (name ,description ,rate,rest_id) values ('panner Bhurji','panner bhurji' , '10' ,1);
+insert into dish (name ,description ,rate,rest_id) values ('panner butter','panner butter' , '11' ,1);
+
+insert into driver(full_name, driver_status, details) values ('kerin shah','available','kerin');
+insert into driver(full_name, driver_status, details) values ('Nisarg shah','available','shah');
+insert into driver(full_name, driver_status, details) values ('Nisarg Patel','available','Nisarg');
