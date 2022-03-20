d3d829f301e46e5b27f8b93c47f726aa5106fb27
kerin shah <kerinshah@kerins-MacBook-Pro-2.local>
2022-03-20 14:32:50 -0400

    config server added

---
+++ config-server/src/main/java/com/uber/config/server/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.uber.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
