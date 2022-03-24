8967543199e51f685efa942f21be4e16433c8cc3
kerin shah <kerinshah@kerins-MacBook-Pro-2.local>
2022-03-24 19:23:11 -0400

    kafka producer added to rest-server for incoming order topic

--- rest-server/src/main/java/com/rest/server/App.java
+++ rest-server/src/main/java/com/rest/server/App.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  *
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class);
--- rest-server/src/main/java/com/rest/server/messaging/SendToIncomingOrderQueue.java
+++ rest-server/src/main/java/com/rest/server/messaging/SendToIncomingOrderQueue.java
@@ -2,11 +2,15 @@ package com.rest.server.messaging;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.jms.MapMessage;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Slf4j
 @Component
@@ -15,8 +19,24 @@ public class SendToIncomingOrderQueue {
     @Autowired
     JmsTemplate jmsTemplate;
 
+    @Autowired
+    Environment environment;
+
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+
     @Transactional
     public void sendOrderId(int orderId) {
+
+        if( Arrays.asList(environment.getActiveProfiles()).contains("kafka")){
+            sendOrderIdKafka(orderId);
+        }else {
+//            sendOrderIdJMS(orderId);
+            sendOrderIdKafka(orderId);
+        }
+    }
+
+    public void sendOrderIdJMS(int orderId) {
         log.info("Sending to Incoming Order Queue");
 
         jmsTemplate.send("incoming_order_queue", session -> {
@@ -25,4 +45,12 @@ public class SendToIncomingOrderQueue {
             return mapMessage;
         });
     }
+
+    public void sendOrderIdKafka(int orderId) {
+        log.info("Sending to Incoming Order Queue");
+
+        kafkaTemplate.send("incoming_order_queue", String.valueOf(orderId));
+    }
+
+
 }
