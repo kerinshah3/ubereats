45241efbc3d215d0d8b3f85d12dabe96c9b14ecd
kerin shah <kerinshah@kerins-MacBook-Pro-2.local>
2022-04-07 00:04:41 -0400

    payment-processed-restaurant consumer add

---
+++ payment-processed-restaurant-consumer/src/main/java/com/kerin/uber/consumer/PaymentProcessedConsumer.java
@@ -0,0 +1,19 @@
+package com.kerin.uber.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@ConditionalOnProperty(name="listener.kafka" , havingValue = "true")
+public class PaymentProcessedConsumer {
+
+    @KafkaListener(topics = "payment_processed_topic",groupId = "email-payment-status-1")
+    public void getProcessedPayment(String orderId){
+        log.info("Received Order Id : {}" ,orderId);
+
+        // TODO: Call a APi for Sending Email
+    }
+}
---
+++ payment-processed-restaurant-consumer/src/main/java/com/kerin/uber/entity/Payment.java
@@ -0,0 +1,33 @@
+package com.kerin.uber.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "PAYMENT")
+@Getter
+@Setter
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PMT_ID", nullable = false)
+    private Integer id;
+
+    @Column(name = "PMT_OPT_ID")
+    private Integer pmtOptId;
+
+    @Column(name = "ORDER_ID")
+    private Integer orderId;
+
+    @Column(name = "CARD_NO", length = 16)
+    private String cardNo;
+
+    @Column(name = "EXP_DATE")
+    private LocalDate expDate;
+
+    @Column(name = "CVV", length = 3)
+    private String cvv;
+}
